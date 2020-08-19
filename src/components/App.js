0556de1bb235bf77eb0f86421a8df265cc582382
Tina Safar Abadi <tinasafarabadi@Tinas-MBP.localdomain>
2020-08-19 17:04:04 -0400

    Dashboar/UI

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {handleInitialData} from "../Actions/shared";
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
+import Dashboard from "./Dashboard";
 
 class App extends Component {
     componentDidMount() {
@@ -14,7 +14,12 @@ class App extends Component {
             <Router>
                 <div>
                     <Route/>
-                    Starter Code
+                    <div className='App'>
+
+                    </div>
+                    {this.props.loading === true
+                        ? null
+                        : <Dashboard />}
                 </div>
             </Router>
 
@@ -22,4 +27,10 @@ class App extends Component {
     }
 }
 
-export default connect()(App)
+function mapStateToProps({ authUser }) {
+    return {
+        loggedIn: authUser !== null,
+    };
+}
+
+export default connect(mapStateToProps)(App);
